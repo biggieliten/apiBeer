69ac02cffa60ec0110aa0e23e83c78b32be341e7
biggieliten <filipisaksson00@gmail.com>
2023-12-19 11:04:24 +0100

    testing

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+let BASE_URL = "https://api.punkapi.com/v2/beers ";
+console.log(BASE_URL);
+// async function getBeer(): Promise<void> {
+//     try {
+//         function fetch(input: RequestInfo | URL, init?: RequestInit): Promise<Response>
+//         console.log(fetch);
+//         if (fetch.status === 200) {
+//             const data: Exempel[] = await fetch.json();
+//             console.log(data);
+//         } else {
+//             throw Error('Något gick fel, försök igen senare');
+//         }
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+// getBeer();
